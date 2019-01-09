3188a3eafaecc705939f09ce6b6d573b0bfebfea
Kathy <catherine.omondi2@gmail.com>
2019-01-10 02:11:11 +0300

    [Feature #163095199] Made changes to validations

--- app/api/v1/utils/validations.py
+++ app/api/v1/utils/validations.py
@@ -69,4 +69,10 @@ class Validations:
         else:
             return False
 
+    @staticmethod
+    def list_iterator(list):
+        for i in list:
+            if i is None or not i:
+                return False
+
     
