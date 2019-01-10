e15729b23809d0047eccf4e8a56b4b86af0a1633
Kathy <catherine.omondi2@gmail.com>
2019-01-10 10:37:18 +0300

    update run.py

--- run.py
+++ run.py
@@ -2,5 +2,9 @@ from app import create_app
 
 app = create_app('development')
 
+@app.route('/')
+def index():
+    return "<p>Find documentation at <a href="https://documenter.getpostman.com/view/5582682/RznFpxuQ">here</a><p>"
+
 if __name__ == "__main__":
     app.run(debug=True)
