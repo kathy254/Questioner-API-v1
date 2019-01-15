55ca25762bb30ddd888a2f2d485888d50b984979
Kathy <catherine.omondi2@gmail.com>
2019-01-16 01:00:43 +0300

    [Chore #163220594] Implement OOP in test_users.py file

--- tests/v1/test_users.py
+++ tests/v1/test_users.py
@@ -6,17 +6,42 @@ from app.api.v1.models.user_models import Members
 
 signup_url = "api/v1/users/signup"
 login_url = "api/v1/users/login"
-signup_payload = {"first_name": "catherine", "last_name": "faith", "other_name": "Were",
-                  "email": "me345@gmail.com", "phone_number": "0303030", "username": "cathy",
-                  "password": "mypass"
-                  }
 
 
 class TestUser(BaseTest):
 
+    def setUp(self):
+        super().setUp()
+        self.signup_payload = {"first_name": "catherine", "last_name": "faith", "other_name": "Were",
+                               "email": "me345@gmail.com", "phone_number": "0303030", "username": "cathy",
+                               "password": "mypass"
+                               }
+
+        self.whitespace_payload = {"first_name": " ", "last_name": "faith", "other_name": "Were",
+                                   "email": "me345@gmail.com", "phone_number": "0303030",
+                                   "username": "cathy", "password": "mypass"
+                                   }
+
+        self.invalid_email = {"first_name": "catherine", "last_name": "faith", "other_name": "Were",
+                              "email": "me345.com", "phone_number": "0303030", "username": "cathy",
+                              "password": "mypass"
+                              }
+
+        self.invalid_password = {"first_name": "catherine", "last_name": "faith",
+                                 "other_name": "Were", "email": "me345@gmail.com",
+                                 "phone_number": "0303030", "username": "cathy", "password": "me"
+                                 }
+
+    def tearDown(self):
+        super().tearDown()
+        self.signup_payload = None
+        self.whitespace_payload = None
+        self.invalid_email = None
+        self.invalid_password = None
+
     def test_signup(self):
         with self.client:
-            response = self.client.post(signup_url, data=json.dumps(signup_payload), content_type="application/json")
+            response = self.client.post(signup_url, data=json.dumps(self.signup_payload), content_type="application/json")
             result = json.loads(response.data.decode("UTF-8"))
 
             self.assertEqual(result["status"], 201)
@@ -32,9 +57,36 @@ class TestUser(BaseTest):
             self.assertEqual(result1["message"], "Please fill out all the fields")
             self.assertEqual(response1.status_code, 406)
             self.assertTrue(response1.content_type == "application/json")
-    
+
+    def test_whitespace_data(self):
+        with self.client:
+            response1 = self.client.post(signup_url, data=json.dumps(self.whitespace_payload), content_type="application/json")
+            result1 = json.loads(response1.data.decode("UTF-8"))
+
+            self.assertEqual(result1["message"], "Data cannot contain whitespaces only")
+            self.assertEqual(response1.status_code, 406)
+            self.assertTrue(response1.content_type == "application/json")
+
+    def test_incorrect_email(self):
+        with self.client:
+            response1 = self.client.post(signup_url, data=json.dumps(self.invalid_email), content_type="application/json")
+            result1 = json.loads(response1.data.decode("UTF-8"))
+
+            self.assertEqual(result1["message"], "Please enter a valid email address")
+            self.assertEqual(response1.status_code, 406)
+            self.assertTrue(response1.content_type == "application/json")
+
+    def test_incorrect_password(self):
+        with self.client:
+            response1 = self.client.post(signup_url, data=json.dumps(self.invalid_password), content_type="application/json")
+            result1 = json.loads(response1.data.decode("UTF-8"))
+
+            self.assertEqual(result1["message"], "Password should be at least 6 characters long")
+            self.assertEqual(response1.status_code, 406)
+            self.assertTrue(response1.content_type == "application/json")
+
     def test_existing_username(self):
-            self.client.post(signup_url, data=json.dumps(signup_payload), content_type="application/json")
+            self.client.post(signup_url, data=json.dumps(self.signup_payload), content_type="application/json")
 
             new_user = {
                 "first_name": "cat",
@@ -55,7 +107,7 @@ class TestUser(BaseTest):
 
     def test_existing_email(self):
 
-        self.client.post(signup_url, data=json.dumps(signup_payload), content_type="application/json")
+        self.client.post(signup_url, data=json.dumps(self.signup_payload), content_type="application/json")
 
         payload3 = {
             "first_name": "not",
@@ -75,7 +127,7 @@ class TestUser(BaseTest):
 
     def test_user_login(self):
 
-        self.client.post(signup_url, data=json.dumps(signup_payload), content_type="application/json")
+        self.client.post(signup_url, data=json.dumps(self.signup_payload), content_type="application/json")
 
         login_payload = {"username": "cathy", "password": "mypass"}
         response4 = self.client.post(login_url, data=json.dumps(login_payload), content_type="application/json")
@@ -87,7 +139,7 @@ class TestUser(BaseTest):
         self.assertTrue(response4.content_type == "application/json")
 
     def test_incorrect_username(self):
-        self.client.post(signup_url, data=json.dumps(signup_payload), content_type="application/json")
+        self.client.post(signup_url, data=json.dumps(self.signup_payload), content_type="application/json")
 
         incorrect_payload = {"username": "faith", "password": "faithuser"}
         response5 = self.client.post(login_url, data=json.dumps(incorrect_payload), content_type="application/json")
@@ -98,8 +150,8 @@ class TestUser(BaseTest):
         self.assertEqual(response5.status_code, 404)
         self.assertTrue(response5.content_type == "application/json")
 
-    def test_incorrect_password(self):
-        self.client.post(signup_url, data=json.dumps(signup_payload), content_type="application/json")
+    def test_wrong_login_password(self):
+        self.client.post(signup_url, data=json.dumps(self.signup_payload), content_type="application/json")
 
         incorrect_password = {"username": "cathy", "password": "notsure"}
         response6 = self.client.post(login_url, data=json.dumps(incorrect_password), content_type="application/json")
