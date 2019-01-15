06ebec14f47622707100c6ac3da86833133e48e2
Kathy <catherine.omondi2@gmail.com>
2019-01-16 01:32:35 +0300

    [Chore #163220594] Refactor code to ensure it meets pep8 and codacy standards

--- tests/v1/test_questions.py
+++ tests/v1/test_questions.py
@@ -13,7 +13,7 @@ downvote_url = "/api/v1/questions/1/downvote"
 class TestQuestions(BaseTest):
     def setUp(self):
         super().setUp()
-        self.question_payload = {"createdBy": 1, "meetup_id": 1, "title": "Git commands", 
+        self.question_payload = {"createdBy": 1, "meetup_id": 1, "title": "Git commands",
                                  "body": "What is the difference between git pull and git fetch?",
                                  "votes": 0
                                  }
@@ -23,7 +23,7 @@ class TestQuestions(BaseTest):
                              "votes": 0
                              }
 
-        self.whitespace_payload = {"createdBy": 1, "meetup_id": 1, "title": "  ", 
+        self.whitespace_payload = {"createdBy": 1, "meetup_id": 1, "title": "  ",
                                    "body": "What is the difference between git pull and git fetch?",
                                    "votes": 0
                                    }
@@ -50,7 +50,7 @@ class TestQuestions(BaseTest):
     def test_whitespace_data(self):
         with self.client:
             response = self.client.post(post_question_url, data=json.dumps(self.whitespace_payload),
-                        content_type="application/json")
+                                        content_type="application/json")
             result = json.loads(response.data.decode("UTF-8"))
 
             self.assertEqual(result["message"], "Data cannot contain whitespaces only")
@@ -71,18 +71,16 @@ class TestQuestions(BaseTest):
         self.assertEqual(response["response"], "Question not found")
         self.assertEqual(result.status_code, 404)
 
-
     def test_upvote_question(self):
         with self.client:
-            self.client.post(post_question_url, data=json.dumps(self.question_one), content_type = "application/json")
+            self.client.post(post_question_url, data=json.dumps(self.question_one), content_type="application/json")
 
-            response = self.client.patch(upvote_url, content_type = "application/json")
+            response = self.client.patch(upvote_url, content_type="application/json")
             self.assertEqual(response.status_code, 200)
 
     def test_downvote_question(self):
         with self.client:
-            self.client.post(post_question_url, data=json.dumps(self.question_one), content_type = "application/json")
+            self.client.post(post_question_url, data=json.dumps(self.question_one), content_type="application/json")
 
             response2 = self.client.patch(downvote_url, content_type="application/json")
             self.assertEqual(response2.status_code, 200)
-
